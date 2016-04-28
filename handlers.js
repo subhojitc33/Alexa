bcb2c988cab345f38aca81b83eb6b1f51f920aaa
ccoenraets <ccoenraets@gmail.com>
2016-04-28 13:27:59 -0400

    first commit

---
+++ handlers.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var count = 1;
+
+exports.UpdateIntent = function () {
+    var text;
+    if (count==1) {
+        text = "There are no new listings and no price changes today.";
+    } else if (count==2) {
+        text = "There is one new listing today. " +
+            "New House for sale: 245 Summer Street in Lexington. 3 bedrooms, 2 bathrooms. $540,000.";
+    } else if (count==3) {
+        text = "There is one price change and one new listing today. " +
+            "Price Reduction: 180 Harrison avenue in Boston was reduced from $650000 to $620000. " +
+            "New House for sale: 245 Summer Street in Lexington. 3 bedrooms, 2 bathrooms. $540,000.";
+    }
+    count++;
+    return text;
+};
---
+++ server.js
@@ -0,0 +1,51 @@
+"use strict";
+
+let express = require('express'),
+    bodyParser = require('body-parser'),
+    handlers = require('./handlers'),
+    app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.json());
+
+app.post('/dreamhouse', (req, res) => {
+
+    if (req.body.request.type === 'LaunchRequest') {
+        alexa.launchRequest(req.body);
+        // TODO For now, we don't care about the session or the user id, we will refactor this later.
+        let sessionId = alexa.sessionId;
+        let userId = alexa.userId;
+        let text = "Welcome to DreamHouse"
+        alexa.response(text, {
+            title: 'DreamHouse',
+            subtitle: 'Welcome to Dreamhouse',
+            content: 'Some commands are "Ask DreamHouse for an update"'
+        }, false, function (error, response) {
+            if (error) {
+                return res.status(500).jsonp({message: error});
+            }
+            return res.jsonp(response);
+        });
+    } else if (req.body.request.type === 'IntentRequest') {
+        alexa.intentRequest(req.body);
+        let handler = handlers[alexa.intentName];
+        let text = handler ? handler() : "I don't know what you said";
+        alexa.response(text, {
+            title: 'DreamHouse',
+            subtitle: 'Subtitle',
+            content: text
+        }, true, function (error, response) {
+            if (error) {
+                return res.status(400).jsonp({message: error});
+            }
+            return res.jsonp(response);
+        });
+
+    }
+
+
+});
+
+app.listen(app.get('port'), function() {
+    console.log("Dreamhouse Alexa server listening on port " + app.get('port'));
+});

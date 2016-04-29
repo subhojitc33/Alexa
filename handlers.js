5724e2662aa38901ba928175fc67df76b2ca1d47
ccoenraets <ccoenraets@gmail.com>
2016-04-29 12:19:46 -0400

    changes

--- handlers.js
+++ handlers.js
@@ -39,38 +39,30 @@ exports.UpdateIntent = function () {
 //};
 
 exports.SearchHouses = (slots, session, response) => {
-    console.log("SearchHouses");
     session.attributes.stage = "ask_city";
     response.ask("OK, in what city?");
 };
 
 exports.AnswerCity = (slots, session, response) => {
-    console.log("AnswerCity");
-    console.log(slots);
-    console.log(session.attributes);
     if (session.attributes.stage === "ask_city") {
         session.attributes.city = slots.City.value;
-        session.attributes.stage = "ask_price_range";
-        response.ask("What price range?");
+        session.attributes.stage = "ask_bedrooms";
+        response.ask("How many bedrooms?");
     }
 };
 
-exports.AnswerPriceRange = (slots, session, response) => {
-    console.log("AnswerPriceRange");
-    console.log(slots);
-    console.log(session.attributes);
-    if (session.attributes.stage === "ask_price_range") {
-        session.attributes.min = slots.min.value;
-        session.attributes.max = slots.max.value;
-        response.say(`OK, looking in ${session.attributes.city} between ${session.attributes.min} and ${session.attributes.max}`);
+exports.AnswerNumber = (slots, session, response) => {
+    if (session.attributes.stage === "ask_bedrooms") {
+        session.attributes.bedrooms = slots.NumericAnswer.value;
+        session.attributes.stage = "ask_price";
+        response.ask("Around what price?");
+    } else if (session.attributes.stage === "ask_price") {
+        session.attributes.price = slots.NumericAnswer.value;
+        response.say(`OK, here is what I found in ${session.attributes.city} around ${session.attributes.price}`);
     }
-
 };
 
+exports.Changes = (slots, session, response) => {
+    response.say("OK, here are the recent changes in " + slots.City.value);
 
-
-
-exports.Changes = () => {
-    console.log(alexa.slots);
-    return "OK, looking in " + slots.City.value;
 }

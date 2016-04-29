a019fb2dbc612fdbc66f103c8c6f79dfb8e0c4f3
ccoenraets <ccoenraets@gmail.com>
2016-04-28 20:59:08 -0400

    adding search

--- server.js
+++ server.js
@@ -12,36 +12,46 @@ app.use(bodyParser.json());
 app.post('/dreamhouse', (req, res) => {
 
     if (req.body.request.type === 'LaunchRequest') {
-        alexa.launchRequest(req.body);
-        // TODO For now, we don't care about the session or the user id, we will refactor this later.
-        let sessionId = alexa.sessionId;
-        let userId = alexa.userId;
-        let text = "Welcome to DreamHouse"
-        alexa.response(text, {
-            title: 'DreamHouse',
-            subtitle: 'Welcome to Dreamhouse',
-            content: 'Some commands are "Ask DreamHouse for an update"'
-        }, false, function (error, response) {
-            if (error) {
-                return res.status(500).jsonp({message: error});
-            }
-            return res.jsonp(response);
-        });
+        //alexa.launchRequest(req.body);
+        //// TODO For now, we don't care about the session or the user id, we will refactor this later.
+        //let sessionId = alexa.sessionId;
+        //let userId = alexa.userId;
+        //let text = "Welcome to DreamHouse"
+        //alexa.response(text, {
+        //    title: 'DreamHouse',
+        //    subtitle: 'Welcome to Dreamhouse',
+        //    content: 'Some commands are "Ask DreamHouse for an update"'
+        //}, false, function (error, response) {
+        //    if (error) {
+        //        return res.status(500).jsonp({message: error});
+        //    }
+        //    return res.jsonp(response);
+        //});
     } else if (req.body.request.type === 'IntentRequest') {
-        alexa.intentRequest(req.body);
+
+        let intent = req.request.intent.name;
         let handler = handlers[alexa.intentName];
         let text = handler ? handler() : "I don't know what you said";
-        alexa.response(text, {
-            title: 'DreamHouse',
-            subtitle: 'Subtitle',
-            content: text
-        }, true, function (error, response) {
-            if (error) {
-                return res.status(400).jsonp({message: error});
+
+        return res.json({
+            //version: this.version,
+            sessionAttributes: {},
+            response: {
+                outputSpeech: {
+                    type: 'PlainText',
+                    text: text
+                },
+                card: {
+                    type: 'Simple',
+                    title: "title",
+                    subtitle: "subtitle",
+                    content: "content"
+                },
+                shouldEndSession: true
             }
-            return res.jsonp(response);
         });
     }
+
 
 
 });
